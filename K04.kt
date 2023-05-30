77ab5f21f5eabfa1c33b4c781236c054b85818b1
RonnieRT <98645458+RonnieRT@users.noreply.github.com>
2023-05-30 22:23:29 +0530

    The Cheaper Cab
    Question Link  :- codechef.com/problems/CABS?tab=submissions

---
+++ K04.kt
@@ -0,0 +1,28 @@
+import java.util.*
+
+fun main(args : Array<String>){
+    getInputAndSolve()
+}
+
+fun getInputAndSolve(){
+    val scanner = Scanner(System.`in`)
+    val casesCount = scanner.nextInt()
+    
+    for(i in 1..casesCount){
+        val first = scanner.nextInt()
+        val second = scanner.nextInt()
+        
+        printOutput(first, second)
+    }
+}
+
+
+fun printOutput(first : Int, second : Int){
+    if(first < second){
+        println("first")
+    }else if(first > second){
+        println("second")
+    }else{
+        println("Any")
+    }
+}
