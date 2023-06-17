cc79bd3bec489d8d46c25f8c1e010a225ffb1f5e
RonnieRT <98645458+RonnieRT@users.noreply.github.com>
2023-06-18 00:30:23 +0530

    Reverse Integer
    Question Link :- https://leetcode.com/problems/reverse-integer/description/

--- K14.kt
+++ K14.kt
@@ -1,5 +1,12 @@
 class Solution {
     fun reverse(x: Int): Int {
-        
+        var num = x
+        var res = 0
+        while (num != 0) {
+            res = res * 10 + num % 10
+            if (res % 10 != num % 10) return 0
+            num /= 10
+        }
+        return res
     }
 }
